3dfa220de2ec67a18375eb3153f9a424a8fc1e13
gracie <150959741+lorahackworth@users.noreply.github.com>
2024-02-07 14:25:43 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Map<String, ArrayList<Double>> myCityTempMap = new HashMap<>();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a city of your choice (OR TYPE END TO END): ");
+        String userChoice = input.nextLine();
+
+        while(!(userChoice.equals("END"))){
+            myCityTempMap.put(userChoice, new ArrayList<Double>(0));
+            System.out.println("Enter a city of your choice (OR TYPE END TO END): ");
+            userChoice = input.nextLine();
+        }
+        System.out.println(myCityTempMap);
+
+        for (int i = 0; i < myCityTempMap.size(); i++) {
+            temperature(myCityTempMap);
+
+        }
+
     }
+
+    static void temperature(Map<String,ArrayList<Double>> a){
+
+        for (int i = 0; i < 5; i++) {
+
+        }
+
+    }
+
 }
+
+
